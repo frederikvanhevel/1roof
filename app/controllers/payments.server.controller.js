48017d4b5bb6e0d5e01507fd4f276071bda8857d
freek <frederik@adhese.eu>
2014-08-28 13:39:21 +0200

    Payments cleanup

--- app/controllers/payments.server.controller.js
+++ app/controllers/payments.server.controller.js
@@ -17,14 +17,9 @@ function createCustomer(user, plan, card, done) {
   };
   if (card) options.card = card;
 
-  console.log(options);
-
   stripe.customers.create(options,
     function(err, customer) {
-      console.log(customer);
-
       if (err) return done(err);
-      console.log(err);
 
       user.customerToken = customer.id;
       user.subscriptionPlan = plan;
@@ -44,15 +39,13 @@ function createCustomer(user, plan, card, done) {
 
 function createSubscription(user, plan, done) {
   stripe.customers.createSubscription(
-    user.customerToken + '',
+    user.customerToken,
     { plan: plan },
     function(err, subscription) {
       if (err) done(err);
 
-      console.log(user.customerToken);
-      console.log(plan);
-      console.log(subscription);
-      user.subscriptionToken = subscription;
+      user.subscriptionPlan = plan;
+      user.subscriptionToken = subscription.id;
 
       user.save(function(err) {
         if (err) {
@@ -92,10 +85,7 @@ exports.choosePlan = function(req, res, next) {
   var plan = req.body.plan;
   var card = req.body.card;
 
-  console.log(plan);
-
   if (!user.customerToken) {
-    console.log('no customerToken, creating customer');
     createCustomer(user, plan, card, function(err) {
       if (err) {
           res.send(400, err);
@@ -105,7 +95,6 @@ exports.choosePlan = function(req, res, next) {
     });
   } else {
     if (!user.subscriptionToken) {
-      console.log('has customerToken, creating subscription');
       createSubscription(user, plan, function(err) {
         if (err) {
             res.send(400, err);
@@ -115,7 +104,6 @@ exports.choosePlan = function(req, res, next) {
       });
     }
     else {
-      console.log('has subscription, changing');
       changeSubscription(user, plan, function(err) {
         if (err) {
             res.send(400, err);
