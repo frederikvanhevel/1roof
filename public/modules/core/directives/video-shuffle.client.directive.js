44511306ef95741fd1b77eddbdc85b6dbdf895b0
freek <frederik@adhese.eu>
2014-08-29 13:43:03 +0200

    Video background shuffle and preloading

--- public/modules/core/directives/video-shuffle.client.directive.js
+++ public/modules/core/directives/video-shuffle.client.directive.js
@@ -1,78 +1,47 @@
 'use strict';
 
-angular.module('core').directive('backgroundVideo', [ '$window',
+angular.module('core').directive('videoShuffle', [ '$window',
   function($window) {
     return {
-      template: '<video id="backgroundVideo" autoplay preload type="" src=""></video>' +
-                '<video id="stagingVideo" autoplay preload type="" src=""></video>',
-      restrict: 'E',
+      template: '<video class="background-video" id="backgroundVideo" autoplay preload type="" src=""></video>' +
+                '<video class="background-video" id="stagingVideo" autoplay preload type="" style="opacity:0;" src=""></video>',
+      restrict: 'A',
       link: function postLink(scope, element, attrs) {
 
         var videoTimer = null;
         
         //Clips for the videos
         var clips = [
-          // ['bike', 9900],
+          ['bike', 9900],
           ['bikefar', 4900],
           ['house1', 2900],
           ['jonnyskateboard', 5900],
           ['kidshouse', 1900],
           ['leahdog', 4900],
           ['majesticdog', 4900],
-          ['trackingdriveway', 4900]
-          // ['trackingstreet', 3900]
+          ['trackingdriveway', 4900],
+          ['trackingstreet', 3900]
         ];
 
         var settings = {
-          'videoContainer': element.find('#backgroundVideo'),
-          'stagingContainer': element.find('#stagingVideo'),
-          // 'videoWebM': $('#backgroundVideoWebM'),
-          // 'videoMp4': $('#backgroundVideoMp4'),
-          // 'stagingWebM': $('#stagingVideoWebM'),
-          // 'stagingMp4': $('#stagingVideoMp4'),
-          // 'supportedTypeExt': 'webm'
+          videoContainer: element.find('#backgroundVideo'),
+          stagingContainer: element.find('#stagingVideo')
         };
 
         var currentClip = 2;
 
-        function shuffle() {
-          for (var i = clips.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp = clips[i];
-            clips[i] = clips[j];
-            clips[j] = temp;
-          }
+        function resize() {
+          // get native video size
+          var videoHeight = settings.videoContainer[0].videoHeight,
+              videoWidth = settings.videoContainer[0].videoWidth;
+
+          // get wrapper size
+          var wrapperWidth = element.width();
+
+          var calculatedVideoHeight = wrapperWidth / (videoWidth / videoHeight);
+
+          settings.videoContainer.animate({opacity:1}, 2000);
         }
-
-
-        function throwFullScreen() {
-          var browserHeight = $window.$(window).height(),
-            browserWidth = $window.$(window).width(),
-            videoHeight = 1280,
-            videoWidth = 1920,
-            videoRatio = videoWidth / videoHeight,
-            scaleWidth = browserWidth / videoWidth,
-            scaleHeight = browserHeight / videoHeight,
-            scale = scaleHeight > scaleWidth ? scaleHeight : scaleWidth,
-            fullScreenWidth = scale * videoWidth,
-            fullScreenHeight = scale * videoHeight;
-
-          if(scaleWidth > scaleHeight) {
-            settings.videoContainer.css('width', '100%');
-            settings.videoContainer.css('height', 'auto');
-            settings.stagingContainer.css('width', '100%');
-            settings.stagingContainer.css('height', 'auto');  
-          }
-          else {
-            settings.videoContainer.css('height', '100%');
-            settings.videoContainer.css('width', 'auto');
-            settings.stagingContainer.css('height', '100%');
-            settings.stagingContainer.css('width', 'auto'); 
-          }
-          
-          // $('#videoContainer').animate({opacity:1}, 2000);
-        }
-
 
         function stopVideo() {
           clearTimeout(videoTimer);
@@ -90,7 +59,7 @@ angular.module('core').directive('backgroundVideo', [ '$window',
             settings.videoContainer.attr('type', 'video/mp4;codecs=\'avc1.42E01E, mp4a.40.2\'');
           }
 
-          shuffle();
+          clips = $window._.shuffle(clips);
 
           settings.videoContainer.attr('src', '/modules/core/img/video/' + clips[0][0] + settings.supportedType);
           settings.videoContainer[0].load();
@@ -100,11 +69,8 @@ angular.module('core').directive('backgroundVideo', [ '$window',
           settings.stagingContainer[0].pause();
 
           videoTimer = window.setTimeout(function() {
-            console.log('looping');
             loop();
           }, clips[0][1]);
-
-          return true;
         }
 
         function loop() {
@@ -123,7 +89,8 @@ angular.module('core').directive('backgroundVideo', [ '$window',
             currentTime = clips[currentClip][1];
           }
 
-          if(settings.videoContainer.css('opacity') === 0) {
+          if(settings.videoContainer.css('opacity') === '0') {
+            
             settings.videoContainer[0].play();
             settings.videoContainer.animate({'opacity':1}, 150, function() {
               settings.stagingContainer.animate({'opacity': 0}, 0);
@@ -133,6 +100,7 @@ angular.module('core').directive('backgroundVideo', [ '$window',
             });
           }
           else {
+
             settings.stagingContainer[0].play();
             settings.stagingContainer.animate({'opacity':1}, 150, function() {
               settings.videoContainer.animate({'opacity': 0}, 0);
@@ -149,7 +117,7 @@ angular.module('core').directive('backgroundVideo', [ '$window',
         }
 
         buildInitialVideo();
-        loop();
+        resize();
 
       }
     };
