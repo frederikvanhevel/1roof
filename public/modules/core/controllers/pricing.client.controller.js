22f880bd2ea23c32444660640937625355079e8f
freek <frederik@adhese.eu>
2014-09-02 14:27:37 +0200

    Mails and stuff

--- public/modules/core/controllers/pricing.client.controller.js
+++ public/modules/core/controllers/pricing.client.controller.js
@@ -9,9 +9,6 @@ angular.module('core').controller('PricingController', ['$scope', '$location', '
 
     Meta.setTitle('Upgraden');
 
-    // If user is not signed in then redirect back home
-    if (!Authentication.user) $location.path('/');
-
     $scope.init = function() {
       $scope.message = $stateParams.message;
 
