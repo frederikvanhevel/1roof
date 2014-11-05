9a03e1b299cc38cb04455a91d829e89f71800999
freek <frederik@adhese.eu>
2014-11-05 17:04:26 +0100

    Revert "Force SSL"
    This reverts commit 32d9286dffd573d2e1232c56d73221b1031754e5.

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -3,17 +3,8 @@
 /**
  * Module dependencies.
  */
-var config = require('../../config/config');
-
 exports.index = function(req, res) {
-
-    if(process.env.NODE_ENV === 'production' && req.headers['x-forwarded-proto'] !== 'https') {
-        res.redirect(config.app.host + req.url);
-    } else {
-        res.render('index', {
-            user: req.user || null
-        });
-    }
-
-
+    res.render('index', {
+        user: req.user || null
+    });
 };
