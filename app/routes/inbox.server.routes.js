2e43e3e2cf2cbfd5b4c0c4899746abab6fcfafd4
freek <frederik@adhese.eu>
2014-12-03 14:09:56 +0100

    Mistake

--- app/routes/inbox.server.routes.js
+++ app/routes/inbox.server.routes.js
@@ -6,7 +6,6 @@ module.exports = function(app) {
 
   // Inbox Routes
   app.route('/api/inbox')
-    .post(users.requiresLogin, inbox.create)
     .get(users.requiresLogin, inbox.list);
 
   app.route('/api/inbox/:inboxId')
