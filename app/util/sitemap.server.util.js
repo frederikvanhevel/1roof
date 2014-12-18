9e89f055536f37e2c625f1519472a874d5e7019e
freek <frederik@adhese.eu>
2014-12-18 11:35:48 +0100

    Sitemap revert cities

--- app/util/sitemap.server.util.js
+++ app/util/sitemap.server.util.js
@@ -37,17 +37,17 @@ function getFavorites() {
 function getSearchPages() {
     var links = [];
     var cities = [
-        'Aalst--Belgïe?lat=50.9378101&lng=4.0409517000000506&proximity=7104.5',
-        'Antwerpen--Belgïe?lat=51.2194475&lng=4.40246430000002&proximity=7061.5',
+        'Aalst--Belgïe',
+        'Antwerpen--Belgïe',
         'Brussel--Belgïe',
-        'Mechelen--Belgïe?lat=51.0258761&lng=4.477536200000031&proximity=7091',
-        'Geel--Belgïe?lat=51.16257&lng=4.990839999999935&proximity=7070',
-        'Gent--Belgïe?lat=51.0543422&lng=3.717424299999948&proximity=7086.5',
-        'Genk--Belgïe?lat=50.96613&lng=5.502100000000041&proximity=7100',
-        'Hasselt--Belgïe?lat=50.93069&lng=5.332480000000032&proximity=7105.5',
-        'Kortrijk--Belgïe?lat=50.8194776&lng=3.2577263000000585&proximity=7122.5',
-        'Leuven--Belgïe?lat=50.883333&lng=4.7000000000000455&proximity=7113',
-        'Turnhout--Belgïe?lat=51.3216509&lng=4.93755770000007&proximity=7045.5'
+        'Mechelen--Belgïe',
+        'Geel--Belgïe',
+        'Gent--Belgïe',
+        'Genk--Belgïe',
+        'Hasselt--Belgïe',
+        'Kortrijk--Belgïe',
+        'Leuven--Belgïe',
+        'Turnhout--Belgïe'
     ];
 
     cities.forEach(function(city) {
