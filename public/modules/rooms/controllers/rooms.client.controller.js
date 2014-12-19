2c05cbbda7ad32d72ce75ce151538565c5bef54a
freek <frederik@adhese.eu>
2014-12-19 13:35:00 +0100

    Meta title shorter

--- public/modules/rooms/controllers/rooms.client.controller.js
+++ public/modules/rooms/controllers/rooms.client.controller.js
@@ -165,9 +165,9 @@ angular.module('rooms').controller('RoomsController', ['$rootScope', '$scope', '
             var meta = {};
 
             if (!$scope.room.info.title) {
-                meta.title = $scope.room.location.street + ' - ' + $scope.room.location.city + ' - 1Roof';
+                meta.title = $scope.room.location.street + ' - 1Roof';
             } else {
-                meta.title = $scope.room.info.title + ' - ' + $scope.room.location.city + ' - 1Roof';
+                meta.title = $scope.room.info.title + ' - 1Roof';
                 meta.description = $scope.room.info.description;
             }
 
