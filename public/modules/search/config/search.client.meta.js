5032889374d795f6d389c1392207ca524eed76c6
freek <frederik@adhese.eu>
2015-01-19 10:02:04 +0100

    Typo

--- public/modules/search/config/search.client.meta.js
+++ public/modules/search/config/search.client.meta.js
@@ -5,7 +5,7 @@ angular.module('search').config([ 'MetaProvider',
     function(MetaProvider) {
         MetaProvider
           .when('/search/:address', {
-            title: 'Huur een appartement, huis of kot in in :address',
+            title: 'Huur een appartement, huis of kot in :address',
             description: 'Krijg een overzicht van alle appartementen, huizen, koten en nog veel meer in :address.'
           });
     }
