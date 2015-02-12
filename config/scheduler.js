090f9148acb21134f818fbeddfbb7dffa20ba2a5
freek <frederik@adhese.eu>
2015-02-12 16:09:17 +0100

    Fuck.

--- config/scheduler.js
+++ config/scheduler.js
@@ -14,22 +14,31 @@ exports.start = function() {
 
     var everynight = new schedule.RecurrenceRule();
     everynight.hour = 2;
+    everynight.minute = 0;
+    everynight.second = 30;
 
     schedule.scheduleJob(everynight, roomAvailabilityCheckJob.run);
     schedule.scheduleJob(everynight, cacheJob.run);
 
     var everyMorning = new schedule.RecurrenceRule();
     everyMorning.hour = 7;
+    everyMorning.minute = 0;
+    everyMorning.second = 30;
 
     schedule.scheduleJob(everyMorning, messageCheckJob.run);
 
     var everyAfternoon = new schedule.RecurrenceRule();
     everyAfternoon.hour = 15;
+    everyAfternoon.minute = 0;
+    everyAfternoon.second = 30;
 
     schedule.scheduleJob(everyAfternoon, socialmediaJob.run);
 
     var everyMonth = new schedule.RecurrenceRule();
     everyMonth.dayOfMonth = 1;
+    everyMonth.hour = 10;
+    everyMonth.minute = 0;
+    everyMonth.second = 30;
 
     schedule.scheduleJob(everyMonth, facebook.extendToken);
 };
