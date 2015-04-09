a0f29f69ca23e3059f951f80b1e394194a6bf49b
Frederik Vanhevel <frederik.vanhevel@telenet.be>
2015-04-09 11:30:10 +0200

    Reenable other socket options

--- config/express.js
+++ config/express.js
@@ -37,7 +37,11 @@ module.exports = function(db) {
     var io = require('socket.io').listen(server);
     io.serveClient(false);
     // Do not use websockets right now, doesn't work with our SSL connection
-    io.set('transports', [ 'xhr-polling' ]);
+    io.set('transports', [
+        'htmlfile',
+        'xhr-polling',
+        'polling'
+    ]);
 
     // Start the app by listening on <port>
     server.listen(config.port);
