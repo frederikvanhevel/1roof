cf3c3b54a24b2f76775ee09b07370f28ee40d6c7
Frederik Vanhevel <frederik.vanhevel@telenet.be>
2015-04-10 14:08:53 +0200

    Add responsive ads to view room page

---
+++ public/modules/core/directives/responsive-ad.client.directive.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('core').directive('responsiveAd', [
+
+    function() {
+        return {
+            restrict: 'A',
+            templateUrl: '/modules/core/views/ads/responsive.client.view.html',
+            controller: function(){
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            }
+        };
+    }
+]);
